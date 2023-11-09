74a17f27519c9d9db89180f0efd6ab4cb699f0cf
Simone D'Angelo <sim.dangelo.95@gmail.com>
2023-11-09 23:11:07 +0100

    joins: exercises

--- src/main/scala/part2dataframes/Joins.scala
+++ src/main/scala/part2dataframes/Joins.scala
@@ -1,9 +1,9 @@
 package part2dataframes
 
 import org.apache.spark.sql.SparkSession
-import org.apache.spark.sql.functions.{expr}
+import org.apache.spark.sql.functions.{col, expr, max}
 
-object Joins extends App{
+object Joins extends App {
 
   val spark = SparkSession.builder
     .appName("Aggregations and Grouping")
@@ -23,12 +23,12 @@ object Joins extends App{
     .json("src/main/resources/data/bands.json")
 
   // joins
-  val joinCondition = guitaristsDF.col("band")===bandsDF.col("id")
+  val joinCondition = guitaristsDF.col("band") === bandsDF.col("id")
   val guitaristsBandsDF = guitaristsDF.join(bandsDF, joinCondition, "inner")
 
   // outer joins
   // left outer: everything in the inner join + all the rows in the LEFT table, with nulls in there the data is missing
-    guitaristsDF.join(bandsDF, joinCondition, "left_outer")
+  guitaristsDF.join(bandsDF, joinCondition, "left_outer")
 
   // right outer: everything in the inner join + all the rows in the RIGHT table, with nulls in there the data is missing
   guitaristsDF.join(bandsDF, joinCondition, "right_outer")
@@ -56,9 +56,57 @@ object Joins extends App{
 
   // - option 3 - rename the offending column and keep the data
   val bandsModDF = bandsDF.withColumnRenamed("id", "bandId")
-  guitaristsBandsDF.join(bandsModDF, guitaristsDF.col("band")===bandsModDF.col("bandId"))
+  guitaristsBandsDF.join(bandsModDF, guitaristsDF.col("band") === bandsModDF.col("bandId"))
 
   // using complex types
-  guitaristsDF.join(guitarsDF.withColumnRenamed("id", "guitarId"), expr("array_contains(guitars, guitarID)")).show()
+  guitaristsDF.join(guitarsDF.withColumnRenamed("id", "guitarId"), expr("array_contains(guitars, guitarID)"))
 
+  /**
+   * EXERCISES
+   *
+   * 1. show all employees and their max salary
+   * 2. show all employees who were never managers
+   * 3. find the job title of the best paid 10 employees in the company
+   */
+
+  // reading from a remote DB
+  val driver = "org.postgresql.Driver"
+  val url = "jdbc:postgresql://localhost:5432/rtjvm"
+  val user = "docker"
+  val password = "docker"
+
+  def readTable(tableName: String) = spark.read
+    .format("jdbc")
+    .option("driver", driver)
+    .option("url", url)
+    .option("user", user)
+    .option("password", password)
+    .option("dbtable", s"public.$tableName")
+    .load()
+
+  val employeesDF = readTable("employees")
+  val salariesDF = readTable("salaries")
+  val deptManagerDF = readTable("dept_manager")
+  val titlesDF = readTable("titles")
+//
+//  employeesDF.show()
+//  salariesDF.show()
+//  deptManagerDF.show()
+//  titlesDF.show()
+
+  // Exercise1
+  val maxSalariesPerEmpNoDF = salariesDF.groupBy("emp_no").agg(max("salary").as("maxSalary"))
+  val employeesSalariesDF = employeesDF.join(maxSalariesPerEmpNoDF, "emp_no")
+
+  // Exercise2
+  val empNeverManagersDF = employeesDF.join(deptManagerDF,
+    employeesDF.col("emp_no") === deptManagerDF.col("emp_no"),
+    "left_anti")
+
+  // Exercise3
+  val mostRecentJobTitlesDF = titlesDF.groupBy("emp_no", "title").agg(max("to_date"))
+  val bestPaidEmployeesDF = employeesSalariesDF.orderBy(col("maxSalary").desc).limit(10)
+  val bestPaidJobsDF = bestPaidEmployeesDF.join(mostRecentJobTitlesDF, "emp_no")
+
+  bestPaidJobsDF.show()
 }
